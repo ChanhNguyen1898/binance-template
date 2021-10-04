b48aac90ad8513af238b0e3b516d4d38b26b1349
Nguyen Thi Duy Chanh <chanhntd1898@gmail.com>
2021-10-04 18:49:39 +0700

    update top page41

--- js/common.js
+++ js/common.js
@@ -46,6 +46,13 @@ $(document).ready(function() {
         $(this).siblings('.bn-sdd-dropdown').toggleClass('css-1tsr9u1 css-fxluzf');
         $(this).children('.bn-input-suffix').children('.css-13c2b5p').children().toggleClass('css-1nlwvj5 css-1mv284k');
     });
+    //ToolTip swap pool page
+    $('.btn-tooltip').hover(function () {
+            $(this).prev('.bn-tooltip-box').fadeIn(300);
+        }, function () {
+            $(this).prev('.bn-tooltip-box').fadeOut(300);
+        }
+    );
     //Modal window
     $('.modal-btn-vayno').click(function () { 
         $('.modal-box').fadeIn();
--- js/swap-pool.js
+++ js/swap-pool.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-    //ToolTip swap pool page
-    $('.css-jurv4m').hover(function () {
-            $(this).prev('.bn-tooltip-box').fadeIn(300);
-        }, function () {
-            $(this).prev('.bn-tooltip-box').fadeOut(300);
-        }
-    );
+    
     //Slide Toggle
     $('.css-q1tzec').click(function (e) { 
         $(this).parent().toggleClass('css-1ds8tsl css-oc7lny');
